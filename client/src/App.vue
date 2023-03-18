2ed24f73ee86d6e101936ff38d213f2475e6fd5c
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-03-18 13:59:05 +0100

    config: adds basic client

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import AppNav from './components/AppNav.vue';
+import AppSidebar from './components/AppSidebar.vue';
+import AppFooter from './components/AppFooter.vue';
+</script>
+
+<template>
+  <v-app>
+    <app-nav />
+    <app-sidebar />
+    
+    <v-main id="app-content">
+      <router-view />
+    </v-main>
+
+    <app-footer />
+  </v-app>
+</template>
