ac524216575a9da037033e70493aebe72757e3f4
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-04-14 17:32:56 +0200

    patch: updates footer (v0.12.7)

--- client/src/components/AppFooter.vue
+++ client/src/components/AppFooter.vue
@@ -1,23 +1,13 @@
 <script setup lang="ts">
-import BaseImage from './base/BaseImage.vue';
-import LogoImage from '@/assets/images/ssasy-logo.svg';
 </script>
 
 <template>
   <v-footer app>
-    <span class="mx-1">&copy; ssasy-demo</span>
-
-    <v-spacer />
-    
     <a
       href="https://www.github.com/ssasy-auth"
       target="_blank"
       class="hide-link">
-      <base-image
-        :src="LogoImage"
-        alt="ssasy logo"
-        height="20px"
-        class="mx-1"/>
+      <span class="mx-1">ssasy-demo &copy; 2023</span>
     </a>
   </v-footer>
 </template>
